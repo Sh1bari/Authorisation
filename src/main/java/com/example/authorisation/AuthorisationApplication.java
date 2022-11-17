89dad263bc2a764ffb829e34bcf573c1d30f4afd
TheKelp1e <vova_krasnov_2004@mail.ru>
2022-11-17 03:07:36 +0300

    Initial commit

---
+++ src/main/java/com/example/authorisation/AuthorisationApplication.java
@@ -0,0 +1,13 @@
+package com.example.authorisation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorisationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorisationApplication.class, args);
+	}
+
+}
