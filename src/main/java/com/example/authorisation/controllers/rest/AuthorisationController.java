f686cbeffafc757c7fa1ccb203b151de55d70977
TheKelp1e <vova_krasnov_2004@mail.ru>
2022-11-18 01:17:27 +0300

    login form downloaded

--- src/main/java/com/example/authorisation/controllers/rest/AuthorisationController.java
+++ src/main/java/com/example/authorisation/controllers/rest/AuthorisationController.java
@@ -1,7 +1,28 @@
 package com.example.authorisation.controllers.rest;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.authorisation.models.UserAuthorisation;
+import com.example.authorisation.repo.UserAuthorisationRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class AuthorisationController {
+    private final UserAuthorisationRepository userAuthorisationRepository;
+
+    public AuthorisationController(UserAuthorisationRepository userAuthorisationRepository) {
+        this.userAuthorisationRepository = userAuthorisationRepository;
+    }
+
+    @PostMapping("/authorisation")
+    public void authorisation(@RequestParam String login,
+                              @RequestParam String password){
+        UserAuthorisation userAuthorisation = new UserAuthorisation(0, login, password);
+        userAuthorisationRepository.save(userAuthorisation);
+    }
+
+    @RequestMapping (method = RequestMethod.GET, value = "/authorisationForm")
+    public ModelAndView showForm(){
+        return new ModelAndView("authorisationModule/authorisation");
+    }
+
 }
